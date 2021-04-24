e009d7f11fa44a4568a3f64bdaa53c58241729ef
Keefko <kohooutdan@gmail.com>
2021-04-24 09:45:09 +0200

    BP-3 slick-carousel integration, property-slider component, impl. to main page

--- broker-portal/src/app/app.module.ts
+++ broker-portal/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PropertyDetailComponent } from './property-detail/property-detail.component';
+import { PropertySliderComponent } from './property-slider/property-slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     NavbarComponent,
-    PropertyDetailComponent
+    PropertyDetailComponent,
+    PropertySliderComponent
   ],
   imports: [
     BrowserModule,
+    SlickCarouselModule,
     AppRoutingModule
   ],
   providers: [],
