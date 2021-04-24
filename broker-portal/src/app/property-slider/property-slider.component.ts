fdb3fa175fa98c2e29624e1ebd2ece1991ecc3e8
Keefko <kohooutdan@gmail.com>
2021-04-24 09:44:57 +0200

    BP-3 slick-carousel integration, property-slider component, impl. to main page

---
+++ broker-portal/src/app/property-slider/property-slider.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-slider',
+  templateUrl: './property-slider.component.html',
+  styles: [
+  ]
+})
+export class PropertySliderComponent implements OnInit {
+
+  slides = [
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/000000"}
+  ];
+  slideConfig = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    focusOnSelect: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
