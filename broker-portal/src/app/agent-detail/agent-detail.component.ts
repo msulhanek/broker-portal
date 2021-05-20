f4a1f0ef0dc8741925f1ad8070458e716aa32d29
Keefko <kohooutdan@gmail.com>
2021-05-20 20:04:00 +0200

    BP-14 Agent detail, contact form

--- broker-portal/src/app/agent-detail/agent-detail.component.ts
+++ broker-portal/src/app/agent-detail/agent-detail.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskData} from "../interfaces/task-data.model";
+import {PropertyService} from "../services/property.service";
+import {SearchCase} from "../interfaces/search-case.model";
+import {DomSanitizer} from "@angular/platform-browser";
 
 
 @Component({
   selector: 'app-agent-detail',
   templateUrl: './agent-detail.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class AgentDetailComponent implements OnInit {
-
-
-  constructor() { }
+  properties: TaskData[] = [];
+  show: boolean = false;
+  constructor(private propertyService: PropertyService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.loadLatest();
   }
 
+  private loadLatest() {
+    this.propertyService.getSuggested('((visualId:*crt*) AND (authorEmail:*Peter Kopúň*) OR (authorName:*Peter Kopúň*))').subscribe(search => {
+
+      search._embedded.cases.forEach((value) => {
+        this.propertyService.getTask(value.stringId).subscribe(searchRequest => {
+          const tasks: SearchCase[] = searchRequest._embedded.tasks as SearchCase[];
+          this.propertyService.getData(tasks[0].stringId).subscribe(data => {
+            const task = {
+              title: value.title,
+              stringId: value.stringId,
+              taskStringId: tasks[0].stringId,
+              ...this.propertyService.parseData(data)
+            };
+            this.propertyService.getImage(tasks[0].stringId).subscribe(image => {
+              let objectURL = URL.createObjectURL(image);
+              task.image = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+              this.properties.push(task);
+            });
+
+          });
+      });
+      this.show = true;
+    });
+  });
+  }
 }
--- broker-portal/src/app/contact-form/contact-form.component.ts
+++ broker-portal/src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +7,38 @@ import {FormGroup} from "@angular/forms";
 })
 export class ContactFormComponent implements OnInit {
   form: FormGroup;
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', [Validators.required,Validators.minLength(10), Validators.maxLength(10)]],
+      message: ['', Validators.required],
+    });
   }
 
+  submit() {
+    this.form.reset();
+    const message = document.querySelector('#message-send');
+    const alert = document.querySelector('#alert-send');
+    if(!alert.classList.contains('show')){
+        alert.classList.add('show')
+    }
+    message.classList.remove('d-none');
+    message.classList.add('d-block');
+  }
+
+  onClick() {
+    const message = document.querySelector('#message-send');
+    const alert = document.querySelector('#alert-send');
+    if(alert.classList.contains('show')){
+      alert.classList.remove('show')
+    }
+
+    message.classList.remove('d-block');
+    message.classList.add('d-none');
+  }
 }
